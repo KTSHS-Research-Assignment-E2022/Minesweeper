902a50b7c32ace12a376da5f5860a749ce3cd05b
jinnosukeKato <96569057+jinnosukeKato@users.noreply.github.com>
2022-04-26 20:28:31 +0900

    タイルロジック内でゼロが開くのを連鎖させる

--- src/commonMain/kotlin/ktshsResearchAssignmentE2022/com/github/minesweeper/MineSweeperLogic.kt
+++ src/commonMain/kotlin/ktshsResearchAssignmentE2022/com/github/minesweeper/MineSweeperLogic.kt
@@ -2,40 +2,101 @@ package ktshsResearchAssignmentE2022.com.github.minesweeper
 
 import kotlin.random.Random
 
-private fun List<List<TileLogic>>.incAround(int: Int, int2: Int) {
+private fun List<List<TileLogic>>.incAround(x: Int, y: Int) {
     // 左上
-    if (int != 0 && int2 != 0) {
-        if (! this[int - 1][int2 - 1].isMine) this[int - 1][int2 - 1].numOfAroundMines++
+    if (x != 0 && y != 0) {
+        if (!this[x - 1][y - 1].isMine) this[x - 1][y - 1].numOfAroundMines++
     }
     // 真上
-    if (int != 0) {
-        if (!this[int - 1][int2].isMine) this[int - 1][int2].numOfAroundMines++
+    if (x != 0) {
+        if (!this[x - 1][y].isMine) this[x - 1][y].numOfAroundMines++
     }
     // 右上
-    if (int != 0 && this[int - 1].size > int2 + 1) {
-        if (!this[int - 1][int2 + 1].isMine) this[int - 1][int2 + 1].numOfAroundMines++
+    if (x != 0 && this[x - 1].size > y + 1) {
+        if (!this[x - 1][y + 1].isMine) this[x - 1][y + 1].numOfAroundMines++
     }
 
     // 左
-    if (int2 != 0) {
-        if (!this[int][int2 - 1].isMine) this[int][int2 - 1].numOfAroundMines++
+    if (y != 0) {
+        if (!this[x][y - 1].isMine) this[x][y - 1].numOfAroundMines++
     }
     // 右
-    if (this[int].size > int2 + 1) {
-        if (!this[int][int2 + 1].isMine) this[int][int2 + 1].numOfAroundMines++
+    if (this[x].size > y + 1) {
+        if (!this[x][y + 1].isMine) this[x][y + 1].numOfAroundMines++
     }
 
     // 左下
-    if (this.size > int + 1 && int2 != 0) {
-        if (!this[int + 1][int2 - 1].isMine) this[int + 1][int2 - 1].numOfAroundMines++
+    if (this.size > x + 1 && y != 0) {
+        if (!this[x + 1][y - 1].isMine) this[x + 1][y - 1].numOfAroundMines++
     }
     // 真下
-    if (this.size > int + 1) {
-        if (!this[int + 1][int2].isMine) this[int + 1][int2].numOfAroundMines++
+    if (this.size > x + 1) {
+        if (!this[x + 1][y].isMine) this[x + 1][y].numOfAroundMines++
     }
     // 右下
-    if (this.size > int + 1 && this[int + 1].size > int2 + 1) {
-        if (!this[int + 1][int2 + 1].isMine) this[int + 1][int2 + 1].numOfAroundMines++
+    if (this.size > x + 1 && this[x + 1].size > y + 1) {
+        if (!this[x + 1][y + 1].isMine) this[x + 1][y + 1].numOfAroundMines++
+    }
+}
+
+private fun List<List<TileLogic>>.openAround(x: Int, y: Int) {
+    // 左上
+    if (x != 0 && y != 0) {
+        if (this[x - 1][y - 1].numOfAroundMines == 0) {
+            this[x - 1][y - 1].isOpened = true
+            this.openAround(x - 1, y - 1)
+        }
+    }
+    // 真上
+    if (x != 0) {
+        if (this[x - 1][y].numOfAroundMines == 0) {
+            this[x - 1][y].isOpened = true
+            this.openAround(x - 1, y)
+        }
+    }
+    // 右上
+    if (x != 0 && this[x - 1].size > y + 1) {
+        if (this[x - 1][y + 1].numOfAroundMines == 0) {
+            this[x - 1][y + 1].isOpened = true
+            this.openAround(x - 1, y + 1)
+        }
+    }
+
+    // 左
+    if (y != 0) {
+        if (this[x][y - 1].numOfAroundMines == 0) {
+            this[x][y - 1].isOpened = true
+            this.openAround(x, y - 1)
+        }
+    }
+    // 右
+    if (this[x].size > y + 1) {
+        if (this[x][y + 1].numOfAroundMines == 0) {
+            this[x][y + 1].isOpened = true
+            this.openAround(x, y + 1)
+        }
+    }
+
+    // 左下
+    if (this.size > x + 1 && y != 0) {
+        if (this[x + 1][y - 1].numOfAroundMines == 0) {
+            this[x + 1][y - 1].isOpened = true
+            this.openAround(x + 1, y - 1)
+        }
+    }
+    // 真下
+    if (this.size > x + 1) {
+        if (this[x + 1][y].numOfAroundMines == 0) {
+            this[x + 1][y].isOpened = true
+            this.openAround(x + 1, y)
+        }
+    }
+    // 右下
+    if (this.size > x + 1 && this[x + 1].size > y + 1) {
+        if (this[x + 1][y + 1].numOfAroundMines == 0) {
+            this[x + 1][y + 1].isOpened
+            this.openAround(x + 1, y + 1)
+        }
     }
 }
 
@@ -44,7 +105,7 @@ class MineSweeperLogic(column: Int, row: Int, ratio: Int, seed: Int) {
 
     init {
         val connectedList = mutableListOf<TileLogic>()
-        for (i in 1 .. ratio) {
+        for (i in 1..ratio) {
             connectedList.add(TileLogic(true))
         }
 
@@ -62,5 +123,10 @@ class MineSweeperLogic(column: Int, row: Int, ratio: Int, seed: Int) {
                 }
             }
         }
+    }
+
+    fun openTile(x: Int, y: Int) {
+        map[x][y].isOpened = true
+        map.openAround(x, y)
     }
 }
