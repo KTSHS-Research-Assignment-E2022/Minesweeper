c4c216072b29d85cdd3ce3a5a8fe5d750b8d67ff
JinnosukeKato <96569057+jinnosukeKato@users.noreply.github.com>
2022-10-17 14:30:48 +0900

    テストを変更

--- src/jsTest/kotlin/MineSweeperLogicTest.kt
+++ src/jsTest/kotlin/MineSweeperLogicTest.kt
@@ -1,6 +1,7 @@
 import ktshsResearchAssignmentE2022.com.github.minesweeper.MineSweeperLogic
 import org.jetbrains.compose.web.testutils.ComposeWebExperimentalTestsApi
 import org.jetbrains.compose.web.testutils.runTest
+import kotlin.random.Random
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -8,7 +9,19 @@ import kotlin.test.assertEquals
 class MineSweeperLogicTest {
     @Test
     fun testLogic() = runTest {
-        val logic = MineSweeperLogic(2,2,1,1)
-        assertEquals(1, logic.numOfMines)
+        var count = 0
+        for (i in 2..20) {
+            for (n in 1..(i * i)) {
+                val logic = MineSweeperLogic(i, i, n, Random.nextInt())
+                for (x in 0 until i) {
+                    for (y in 0 until i) {
+                        if (logic.map[x][y].isMine) count++
+                    }
+                }
+                println("length: $i, mine:$n")
+                assertEquals(n, count)
+            }
+            count = 0
+        }
     }
 }
